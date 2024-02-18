dbf8c53381ef8a1c4945925d9d00eeb1a5159d7b
Chunarev Sergei <chunarev.se.a@gmail.com>
2024-02-18 21:45:25 +0100

    Initial commit

---
+++ src/main/java/com/memorizing/commonapi/MemorizingCommonApiApplication.java
@@ -0,0 +1,13 @@
+package com.memorizing.commonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizingCommonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizingCommonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/memorizing/commonapi/MemorizingCommonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memorizing.commonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizingCommonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
